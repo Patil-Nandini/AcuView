cae5ce8816a10783618cb0293c4ce86c9ed9fbec
Sundeep <sundeeprebbapragada@gmail.com>
2025-09-13 01:45:29 +0530

    Created the Introducingthefirst page amd made required changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Header from "@/components/Header";
 import Industries from "@/components/IndustriesWeServe";
 import LendersResult from "@/components/LendersResult";
 import DontFundBlind from "@/components/DontFundBlind";
+import IntroducingTheFirst from "@/components/Introducingthefirst";
 
 export default function Home() {
   return (
     <main className="bg-white flex justify-center">
       <div className="w-full max-w-[1920px]">
         {/* <Header /> */}
+        <IntroducingTheFirst />
         <DontFundBlind />
         <LendersResult />
         <Industries/>
