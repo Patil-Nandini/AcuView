e801ff51cb804307e84d28cca51f2bd4cd02ac2e
Patil-Nandini <156404898+Patil-Nandini@users.noreply.github.com>
2025-09-12 22:24:03 +0530

    lenders result updated banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+import ComparisonSection from "@/components/ComparisionSection";
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import FoundingBenefits from "@/components/FoundingMember";
 import Industries from "@/components/IndustriesWeServe";
 import LendersResult from "@/components/LendersResult";
+import SetupSteps from "@/components/SetupSteps";
+import Testimonials from "@/components/Testimonial";
+import WhoisAcuViewBuiltFor from "@/components/WhoisAcuviewBuiltFor";
 
 export default function Home() {
   return (
     <main className="bg-white flex justify-center">
-      <div className="w-full max-w-[1920px]">
+      <div className="w-full ">
         {/* <Header /> */}
         <LendersResult />
+        <WhoisAcuViewBuiltFor/>
+        <FoundingBenefits />
+        <ComparisonSection />
+        <SetupSteps />
+        <Testimonials />
         <Industries/>
         <Footer />
       </div>
